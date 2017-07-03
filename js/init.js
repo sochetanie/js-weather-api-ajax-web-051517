cb9dc150a794e291d7968cd8eb64e67ceff1cbe8
sochetanie <allagorik2901@gmail.com>
2017-07-03 15:33:22 -0400

    done

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "348788d7f586e750";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,41 @@
 function getFahrenheits(result){
   // Your code goes here
+  // debugger 
+  return result.hourly_forecast.map(x=> {
+    return x.temp.english
+  })
 }
 
 function getHours(result){
   // Your code goes here
+  return result.hourly_forecast.map(x=> {
+    return x.FCTTIME.hour
+  })
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  let dataS = {
+    labels: labels,
+    datasets: [{ 
+        label : 'Hourly Weather for New York', 
+        fillColor: 'rgba(220,220,220,0.2)', 
+        strokeColor: 'rgba(220,220,220,1)', 
+        pointColor: 'rgba(220,220,220,1)', 
+        pointStrokeColor: '#fff', 
+        pointHighlightFill: '#fff', 
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: data
+    }]
+   }
+    return dataS
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp', 
+    success: success
+  })
 }
